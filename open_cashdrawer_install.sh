b517654a84ae34771424b86b7a0035b5dc2a2c80
stolz1982 <stolz1982@gmx.de>
2018-11-23 14:56:02 +0100

    1>/dev/null 2>&1 for rm

--- open_cashdrawer_install.sh
+++ open_cashdrawer_install.sh
@@ -4,7 +4,7 @@ KO="kioskoperator"
 SCRIPT_FOLDER="/home/kioskoperator/open_cashdrawer"
 FOLDER="/usr/share/applications"
 FILENAME="open_cashdrawer.desktop"
-/bin/rm $FOLDER/$FILENAME
+/bin/rm $FOLDER/$FILENAME 1>/dev/null 2>&1
 /bin/chmod +x $SCRIPT_FOLDER/open_cashdrawer.sh
 /usr/bin/touch $FOLDER/$FILENAME
 /bin/chown -R $KO:$KO $SCRIPT_FOLDER 
